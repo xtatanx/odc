aeabb69b8d585662e72081c19042ef994f80e536
Daniel Warner <danywarner@gmail.com>
2014-04-23 15:38:59 -0500

    sharing bug fixed

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,20 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.gestionaleauto.plugins.TwitterFacebookSharePlugin/www/GATwitterFacebookShare.js",
-        "id": "com.gestionaleauto.plugins.TwitterFacebookSharePlugin.TwitterFacebookSharePlugin",
-        "clobbers": [
-            "GA.TwitterFacebookSharePlugin"
-        ]
-    },
-    {
-        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
-        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -27,15 +13,21 @@ module.exports = [
         "merges": [
             "plugins.statusBar"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.gestionaleauto.plugins.TwitterFacebookSharePlugin": "1.0.0",
-    "nl.x-services.plugins.socialsharing": "4.0.8",
     "org.apache.cordova.dialogs": "0.2.6",
-    "org.apache.cordova.plugins.StatusBar": "0.1.0"
+    "org.apache.cordova.plugins.StatusBar": "0.1.0",
+    "nl.x-services.plugins.socialsharing": "4.0.8"
 }
 // BOTTOM OF METADATA
 });
