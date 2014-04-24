f687eb0a284cb59be809d464c06c66ed1c3b7e01
Daniel Warner <danywarner@gmail.com>
2014-04-24 11:53:00 -0500

    every plugin works now

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -8,17 +8,17 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.plugins.StatusBar/www/StatusBar.js",
-        "id": "org.apache.cordova.plugins.StatusBar.StatusBar",
-        "merges": [
-            "plugins.statusBar"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
             "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.plugins.StatusBar/www/StatusBar.js",
+        "id": "org.apache.cordova.plugins.StatusBar.StatusBar",
+        "merges": [
+            "plugins.statusBar"
         ]
     }
 ];
@@ -26,8 +26,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "nl.x-services.plugins.socialsharing": "4.0.8",
-    "org.apache.cordova.plugins.StatusBar": "0.1.0",
-    "org.apache.cordova.dialogs": "0.2.7"
+    "org.apache.cordova.dialogs": "0.2.7",
+    "org.apache.cordova.plugins.StatusBar": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
